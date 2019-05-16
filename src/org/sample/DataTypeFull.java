b0fa5076fe26a2fc13042f76c7f3e63003d41c7a
fernandezalbert <albertfernandez48@gmail.com>
2019-05-16 15:02:58 +0530

    Added datatype project

---
+++ src/org/sample/DataTypeFull.java
@@ -0,0 +1,22 @@
+package org.sample;
+
+public class DataTypeFull {
+public static void main(String[] args) {
+	byte b=12;
+	System.out.println(b);
+	short s=1234;
+	System.out.println(s);
+	int i=12345678;
+	System.out.println(i);
+	long l=987644567l;
+	System.out.println(l);
+	char c='#';
+	System.out.println(c);
+	String s1= "Hi Hru";
+	System.out.println(s1);
+	float f=8989289.78f;
+	System.out.println(f);
+	boolean b2=false;
+	System.out.println(b2);
+}
+}
---
+++ src/org/sample/DataTypes.java
@@ -0,0 +1,16 @@
+package org.sample;
+
+public class DataTypes {
+public static void main(String[] args) {
+	int empId=12345;
+	System.out.println("EmpId is:\n"+empId);
+	String name="Albert Fernandez";
+	System.out.println("Name is "+name);
+	long phno=8508676566l;
+	System.out.println("Mobile Number is"+phno);
+	float sal=30000f;
+	System.out.println("Salary is"+sal);
+	String email="albertfernandez48@gmail.com";
+	System.out.println("Email Id is"+email);
+}
+}
